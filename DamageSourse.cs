f31416e327562d58db460f06093d8555bfc16fac
marshme1 <128727579+marshme1@users.noreply.github.com>
2024-10-23 09:32:42 +0300

    Комменты DamageSourse.cs

--- DamageSourse.cs
+++ DamageSourse.cs
@@ -6,10 +6,12 @@ public class DamageSource : MonoBehaviour
 {
     [SerializeField] private int damageAmount = 1;
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.GetComponent<EnemyHealth>()) {
-            EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
-            enemyHealth.TakeDamage(damageAmount);
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<EnemyHealth>())//если попадает по объекту, на котором висит скрипт EnemyHealth, то
+        {
+            EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>(); //инициализируем скрипт EnemyHealth
+            enemyHealth.TakeDamage(damageAmount);//отнимаем здоровье у этого объекта
         }
     }
 }
