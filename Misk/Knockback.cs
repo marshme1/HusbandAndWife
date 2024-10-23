fde74388cc733a166e14c49307e34cd5e71c22a6
marshme1 <128727579+marshme1@users.noreply.github.com>
2024-10-23 15:09:11 +0300

    комментарии Knockback.cs

--- Misk/Knockback.cs
+++ Misk/Knockback.cs
@@ -16,14 +16,14 @@ public class Knockback : MonoBehaviour
 
     public void GetKnockedBack(Transform damageSource, float knockBackThrust) {
         gettingKnockedBack = true;
-        Vector2 difference = (transform.position - damageSource.position).normalized * knockBackThrust * rb.mass;
-        rb.AddForce(difference, ForceMode2D.Impulse); 
-        StartCoroutine(KnockRoutine());
+        Vector2 difference = (transform.position - damageSource.position).normalized * knockBackThrust * rb.mass;//вычисляем вектор, куда нужно отбросить объект после удара от персонажа 
+        rb.AddForce(difference, ForceMode2D.Impulse); //отбрасываем персонажа, который получает удар
+        StartCoroutine(KnockRoutine());//стартует коррутина
     }
 
     private IEnumerator KnockRoutine() {
-        yield return new WaitForSeconds(knockBackTime);
-        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(knockBackTime);//спустя время 
+        rb.velocity = Vector2.zero;//делаем вектор скорости объекта равном нулю
         gettingKnockedBack = false;
     }
 }
